4cbed2dd0efae4dd527c9dd47db8c840730d2dfd
SotoDev0 <javier.soto8934@gmail.com>
2025-01-19 19:31:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jornada_laboral',
+  webDir: 'www'
+};
+
+export default config;
